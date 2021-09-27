3d782c0927c8a99d79ec502df9479b4cd42ea5d2
MultiVR <91363547+MultiVR@users.noreply.github.com>
2021-09-27 15:24:57 +0300

    Update MainScript.cs

--- MainScript.cs
+++ MainScript.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
+[Serializable]
 public class MainScript
 {
-    float hp; // здоровье у игрока
-    string clan; // фракция
-    float speed;
+    public string nickPlayer; // ник игрока
+    public float hp; // здоровье у игрока
+    public string clan; // фракция
+    public float speed;
 
-    float CCP; // CCP(сombat capability points) / ОБ(очки боеспособности)
+    public float CCP; // CCP(сombat capability points) / ОБ(очки боеспособности)
 
-    float damage; //урон у оружия
-    float range; // дальность
-    float accuracy; // точность
-    float scatter; // разброс
-    float fireRate; //скорострельность
+    public float damage; //урон у оружия
+    public float range; // дальность
+    public float accuracy; // точность
+    public float scatter; // разброс
+    public float fireRate; //скорострельность
 
-    float ammo; //всего патронов
-    float[] ammoInClip; //патроны в каждой обойме
-    float lockingTimeClip; // на сколько блокируются обоймы
-    float impactForce; // "сила" стрельбы
+    public float ammo; //всего патронов
+    public float[] ammoInClip; //патроны в каждой обойме
+    public float lockingTimeClip; // на сколько блокируются обоймы
+    public float impactForce; // "сила" стрельбы
 
 }
